cf6277cd55ea3a52c368bb5d35a35aad85a006a3
BRamotowski <bartlomiej.ramotowski@gmail.com>
2024-01-28 16:44:52 +0100

    Update dimention_reduction.R

--- dimentions reduction/dimention_reduction.R
+++ dimentions reduction/dimention_reduction.R
@@ -23,7 +23,7 @@ corrplot(health.cor, order ="alphabet", tl.cex=0.6)
 corrplot(health.cor,method = "number", order ="alphabet", tl.cex=0.6,number.cex = 0.6)
 
 
-# analysis for products
+
 dist.health<-dist(t(health)) # as a main input we need distance between units
 as.matrix(dist.health)[1:10, 1:10] # let’s see the distance matrix
 mds1<-cmdscale(dist.health, k=2) #k - the maximum dimension of the space
@@ -31,3 +31,14 @@ summary(mds1)	# we get coordinates of new points
 
 plot(mds1,xlim=c(-12000,-10000))
 head(mds1)
+
+# k-means
+install.packages("factoextra")
+library(factoextra)
+
+# for features
+#km.f<-eclust(t(CIA), "kmeans", hc_metric="euclidean",k=5) # does not work due to NAs
+km.f<-eclust(t(CIA.new), "kmeans", hc_metric="euclidean",k=5)
+
+fviz_cluster(km.f, main="kmeans / Euclidean", ylim=c(-15, 5), xlim=c(-15, 55), labelsize=10, repel=TRUE)
+fviz_silhouette(km.f)
